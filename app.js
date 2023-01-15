f41c8e07f60db8d236e22ef96e9836ce04ab5921
Jihad Hasan Masum <jihadhasan@Jihads-MacBook-Pro.local>
2023-01-15 15:35:48 +0600

    updated

--- app.js
+++ app.js
@@ -113,7 +113,12 @@ app.get("/", function(req,res){
   }); 
  
 });
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
 
+app.listen(port);
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
